0a0b1b46baf8b802c5e1f98b1dc04d2f4a41bc22
javeeds <smjaveed459@gmail.com>
2025-09-15 04:21:45 +0530

    Swap of two numbers with python internal method

--- swapnumbers.py
+++ swapnumbers.py
@@ -1,5 +1,5 @@
 #Swap of two numbers using temporary variable
-print("Choose 1 for with temp swapping 2 for without temp swapping")
+print("Choose 1 for with temp swapping 2 for without temp swapping, 3 using python annotation")
 choose = int(input("Choose:"))
 if choose == 1:
     a = int(input("Enter a value:"))
@@ -21,6 +21,12 @@ elif choose == 2:
     a = a+b
     b=a-b
     a = a-b
+    print("value of a is:", a)
+    print("value of b is:", b)
+elif choose == 3:
+    a=int(input("Enter a value:"))
+    b=int(input("Enter b value:"))
+    a, b = b, a
     print("value of a is:", a)
     print("value of b is:", b)
 
