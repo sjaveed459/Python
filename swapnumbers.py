3d98fa7b9bc0e2dabba6470bace9da6165e87168
javeeds <smjaveed459@gmail.com>
2025-09-14 18:04:49 +0530

    Swap of two numbers

---
+++ swapnumbers.py
@@ -0,0 +1,29 @@
+#Swap of two numbers using temporary variable
+print("Choose 1 for with temp swapping 2 for without temp swapping")
+choose = int(input("Choose:"))
+if choose == 1:
+    a = int(input("Enter a value:"))
+    b = int(input("Enter b value:"))
+    # taking temporary variable here
+    print("value of a is:", a)
+    print("value of b is:", b)
+    temp = a
+    a = b
+    b = temp
+    print("value of a is:", a)
+    print("value of b is:", b)
+    #Swap of two numbers without using temp variable
+elif choose == 2:
+    a=int(input("Enter a value:"))
+    b=int(input("Enter b value:"))
+    print("value of a is:", a)
+    print("value of b is:", b)
+    a = a+b
+    b=a-b
+    a = a-b
+    print("value of a is:", a)
+    print("value of b is:", b)
+
+
+
+
